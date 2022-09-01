4464d83ff2f81dec078da86fff231e9bcdcacc6e
Desiree Castro <desiree.anne.castro@oocl.com>
2022-09-01 14:08:46 +0800

    test: first test fail for countOff with input 1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,29 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_number_when_countOf_given_1() {
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String expected = "1";
+
+        //when
+        String actual = fizzBuzz.countOff(1);
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+//    @Test
+//    void should_return_number_when_countOf_given_1() {
+//        //given
+//
+//        //when
+//
+//        //then
+//    }
 }
