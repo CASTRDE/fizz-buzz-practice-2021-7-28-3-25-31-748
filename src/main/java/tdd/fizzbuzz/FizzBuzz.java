e46d83348bbf6e5d2a9a3e2cac04319181a857b2
Desiree Castro <desiree.anne.castro@oocl.com>
2022-09-01 14:30:39 +0800

    feat: added handling when countOff input is multiple of 7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,11 +2,13 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int i) {
-        String FIZZ = "Fizz", BUZZ = "Buzz";
+        String FIZZ = "Fizz", BUZZ = "Buzz", WHIZZ = "Whizz";
         if( i%3 == 0)
             return FIZZ;
         else if( i%5 == 0)
             return BUZZ;
+        else if( i%7 == 0)
+            return WHIZZ;
         else
             return String.valueOf(i);
     }
