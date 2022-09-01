e1094925d93f5bf769c17d1fc9965a4e10312318
Desiree Castro <desiree.anne.castro@oocl.com>
2022-09-01 14:13:55 +0800

    test: test fail when countOff input is multiple of 3

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -18,6 +18,19 @@ public class FizzBuzzTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void should_return_fizz_when_countOf_multiple_of_3() {
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String expected = "fizz";
+
+        //when
+        String actual = fizzBuzz.countOff(3);
+
+        //then
+        assertEquals(expected, actual);
+    }
+
 //    @Test
 //    void should_return_number_when_countOf_given_1() {
 //        //given
