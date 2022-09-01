ae33ad8dedd67fc78cbf1a591f12f5a5f27d0ac0
Desiree Castro <desiree.anne.castro@oocl.com>
2022-09-01 14:21:20 +0800

    feat: added handling when countOff is multiple of 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,9 +2,11 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int i) {
-        String FIZZ = "Fizz";
+        String FIZZ = "Fizz", BUZZ = "Buzz";
         if( i%3 == 0)
             return FIZZ;
+        else if( i%5 == 0)
+            return BUZZ;
         else
             return String.valueOf(i);
     }
