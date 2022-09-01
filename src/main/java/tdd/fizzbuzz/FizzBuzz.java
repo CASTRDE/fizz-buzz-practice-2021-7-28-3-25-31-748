1b6974aab3573548ae9141b5f2e00e23ec72a69c
Desiree Castro <desiree.anne.castro@oocl.com>
2022-09-01 14:19:57 +0800

    refactor: assigned the Fizz string value to a string variable for reuse

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,8 +2,9 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int i) {
+        String FIZZ = "Fizz";
         if( i%3 == 0)
-            return "Fizz";
+            return FIZZ;
         else
             return String.valueOf(i);
     }
